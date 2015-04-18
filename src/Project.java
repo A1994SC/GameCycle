c120737bde4b0a06b455e38537982dcdfb1530ca
Allen Conlon <allen.conlon.12@cnu.edu>
2015-04-17 20:27:20 -0400

    'Finished' need to implement better cycle detection.

--- src/Project.java
+++ src/Project.java
@@ -23,13 +23,8 @@ public class Project {
             i++;
             for (int j = 0; j < grph[EDGE]; j++, i++) {
                 int[] relation = Utilities.toIntArray(list.get(i).split(" "));
-                System.out.print(relation[0] + "," + relation[1] + " ");
                 g.add(relation[CHILD], relation[PARENT]);
             }
-//            for (int j = 0; j < grph[EDGE]; j++) {
-            System.out.print(g.hasCycles() + " ");
-//            }
-            System.out.println();
             System.out.println(g.status());
         }
     }
@@ -48,50 +43,35 @@ public class Project {
                 graph[x][y] = MARK;
         }
 
-        public void print() {
-            for (int i = 0; i < graph.length; i++) {
-                for (int j = 0; j < graph.length; j++)
-                    System.out.print(graph[i][j] + " ");
-                System.out.println();
+        public int isLinear() {
+            int count = 0, temp = 0;
+            for (int i = 0; i < size + 1; i++) {
+                temp = 0;
+                for (int j = 0; j < size + 1; j++)
+                    if (graph[j][i] == MARK)
+                        temp++;
+                if (temp > count)
+                    count = temp;
             }
+            return count;
         }
 
-        private final int MAX = Integer.MAX_VALUE;
-        private final int MIN = Integer.MIN_VALUE;
-        private final int maxI = 1;
-        private final int minI = 0;
-
-        public boolean hasCycles() {
-            int[][] dist = new int[2][size + 1];
-            for (int i = 0; i < size; i++)
-                dist[maxI][i] = MAX;
-            for (int i = 0; i < size; i++)
-                dist[minI][i] = MIN;
-
-            for (int k = 1; k <= size; k++)
-                for (int i = 1; i <= size; i++)
-                    for (int j = 1; j <= size; j++) {
-                        if (graph[i][j] != MAX && dist[minI][j] > (dist[minI][i] + graph[i][j]))
-                            dist[minI][i] = (dist[minI][i] + graph[i][j]);
-                        if (graph[i][j] != MIN && dist[maxI][j] < (dist[maxI][i] + graph[i][j]))
-                            dist[maxI][i] = (dist[maxI][i] + graph[i][j]);
-                    }
-
-            boolean flag = false;
-            for (int i = 1; i <= size; i++)
-                for (int j = 1; j <= size; j++) {
-                    if (graph[i][j] != MAX && dist[minI][j] > (dist[minI][i] + graph[i][j]))
-                        flag = true;
-                    if (graph[i][j] != MIN && dist[maxI][j] < (dist[maxI][i] + graph[i][j]))
-                        flag = true;
-                }
-
-            return flag;
+        public boolean isCycle() {
+            for (int i = 0; i < graph.length; i++)
+                for (int j = 0; j < graph.length; j++)
+                    if (graph[i][j] == MARK && graph[j][i] == MARK)
+                        return true;
+            return false;
         }
+
 
         public String status() {
-            print();
-            return "Testing";
+            if (isCycle())
+                return "Infeasible game";
+            if (isLinear() > 1)
+                return "Nonlinear gameplay possible";
+            else
+                return "Linear gameplay possible";
         }
     }
 
